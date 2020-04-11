f014fc9464f6ce7bc54c76c5b9a78b7b3e9a434f
ploidplanp <waoekpr@gmail.com>
2020-04-12 01:29:50 +0700

    add 2 page
    หน้า เพิ่มประกัน, เพิ่มพ.ร.บ.

--- mysite/contract/urls.py
+++ mysite/contract/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('report/expire', views.report_expire, name='report_expire')
+    path('report/expire', views.report_expire, name='report_expire'),
+    path('new/insurance', views.new_policy, name='new_policy'),
+    path('new/compulsory', views.new_compulsory, name='new_compulsory'),
 ]
--- mysite/contract/views.py
+++ mysite/contract/views.py
@@ -5,5 +5,16 @@ from django.template.context_processors import request
 # Create your views here.
 
 @login_required
+# หน้ารายงานกรมธรรม์หมดอายุ
 def report_expire(request):
-    return render(request, 'report_ex.html')
+    return render(request, template_name='report_ex.html')
+
+@login_required
+# หน้าเพิ่มกรมธรรม์ ประกัน
+def new_policy(request):
+    return render(request, template_name='insurance_policy/new_policy.html')
+
+@login_required
+# หน้าเพิ่มกรมธรรม์ พ.ร.บ.
+def new_compulsory(request):
+    return render(request, template_name='compulsory_insurance/new_compulsory.html')
