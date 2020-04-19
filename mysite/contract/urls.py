fc4bb44499aef839b2b519b90d8192b5931ac020
ploidplanp <waoekpr@gmail.com>
2020-04-19 18:10:49 +0700

    หน้ารายการกรมธรรม์
    ค้นหาได้แล้ว

--- mysite/contract/urls.py
+++ mysite/contract/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('report/expire', views.report_expire, name='report_expire'),
     path('new/insurance', views.new_policy, name='new_policy'),
     path('new/compulsory', views.new_compulsory, name='new_compulsory'),
-    path('search', views.contract_search, name="contract_search"),
+    path('search/insurance', views.ins_search, name="ins_search"),
+    path('search/compulsory', views.comp_search, name='comp_search'),
     path('getowner/', views.getowner),
     path('getcar/', views.getcar),
     path('getcus/', views.getcus)
--- mysite/contract/views.py
+++ mysite/contract/views.py
@@ -86,7 +86,16 @@ def new_compulsory(request):
     return render(request, 'compulsory_insurance/new_compulsory.html')
 
 
-def contract_search(request):
+
+# หน้ารายการกรมธรรม์ประกัน
+@login_required
+def ins_search(request):
+    msg = ''
+    searchcontractid = request.POST.get('contractid', '')
+    searchlicense = request.POST.get('license', '')
+    searchoname = request.POST.get('oname', '')
+    searchosur = request.POST.get('osur', '')
+
     userid = request.user.id
     me = Person.objects.get(user_id=userid) #ตัวเรา=userที่ login
     takecare = Customer.objects.filter(seller_id=me.id) #ลูกค้าที่ userดูแล
@@ -96,22 +105,68 @@ def contract_search(request):
     # takecarelistid คือ ไอดีลูกค้าที่ user ดูแล
 
     #กรมธรรม์ทั้งหมดของลูกค้าที่ user ดูแล และเพิ่ม3 วันล่าสุด
-    cusbuycontract = Contract.objects.filter(customer_id__in=takecarelistid, register_date__gte=date.today() - timedelta(days=3))
+    cusbuycontract = Contract.objects.filter(customer_id__in=takecarelistid, register_date__gte=date.today() - timedelta(days=7))
+    cusbuycontractlistid = [] 
+    for i in cusbuycontract:
+        cusbuycontractlistid.append(i) #ไอดีกรมธรรม์ทั้งหมดของลูกค้าที่ user ดูแล
+
+     # print('ลูกค้าที่ซื้อประกัน')
+    cusbuyinsurance = Insurance_Policy.objects.filter(contract_id__in=cusbuycontractlistid, contract__status='Available')
+
+    if request.method == 'POST':
+        if 'search1' in request.POST and searchcontractid != '':
+            cusbuyinsurance = Insurance_Policy.objects.filter(contract_id__in=cusbuycontractlistid, contract__status='Available', insurance_id=searchcontractid)
+            msg = 'ค้นหากรมธรรม์เลขที่ %s' %(searchcontractid)
+            print(cusbuyinsurance)
+        elif 'search2' in request.POST and searchoname != '' and searchosur != '' and searchlicense != '':
+            cusbuyinsurance = Insurance_Policy.objects.filter(contract_id__in=cusbuycontractlistid, contract__status='Available', contract__car__license_on=searchlicense, contract__car__owner__fname=searchoname, contract__car__owner__lname=searchosur)
+            msg = 'ค้นหากรมธรรม์ทะเบียนรถ %s ผู้เอาประกัน %s %s' %(searchlicense, searchoname, searchosur)
+
+    context = {
+        'cusbuyinsurance': cusbuyinsurance,
+        'msg': msg
+    }
+    return render(request, 'insurance_policy/search_insurance.html', context=context)
+
+# หน้ารายการกรมธรรม์พ.ร.บ.
+@login_required
+def comp_search(request):
+    msg = ''
+    searchcontractid = request.POST.get('contractid', '')
+    searchlicense = request.POST.get('license', '')
+    searchoname = request.POST.get('oname', '')
+    searchosur = request.POST.get('osur', '')
+
+    userid = request.user.id
+    me = Person.objects.get(user_id=userid) #ตัวเรา=userที่ login
+    takecare = Customer.objects.filter(seller_id=me.id) #ลูกค้าที่ userดูแล
+    takecarelistid = []
+    for i in takecare:
+        takecarelistid.append(i.id)
+    # takecarelistid คือ ไอดีลูกค้าที่ user ดูแล
+
+    #กรมธรรม์ทั้งหมดของลูกค้าที่ user ดูแล และเพิ่ม3 วันล่าสุด
+    cusbuycontract = Contract.objects.filter(customer_id__in=takecarelistid, register_date__gte=date.today() - timedelta(days=7))
     cusbuycontractlistid = [] 
     for i in cusbuycontract:
         cusbuycontractlistid.append(i) #ไอดีกรมธรรม์ทั้งหมดที่ลูกค้าที่ user ดูแล
 
     # print('ลูกค้าที่ซื้อพรบ')
-    cusbuycoumpulsory = Compulsory_Insurance.objects.filter(contract_id__in=cusbuycontractlistid)
-    # print('ลูกค้าที่ซื้อประกัน')
-    cusbuyinsurance = Insurance_Policy.objects.filter(contract_id__in=cusbuycontractlistid)
+    cusbuycoumpulsory = Compulsory_Insurance.objects.filter(contract_id__in=cusbuycontractlistid, contract__status='Available')
+    
+    if request.method == 'POST':
+        if 'search1' in request.POST and searchcontractid != '':
+            cusbuycoumpulsory = Compulsory_Insurance.objects.filter(contract_id__in=cusbuycontractlistid, contract__status='Available', compulsory_id=searchcontractid)
+            msg = 'ค้นหากรมธรรม์เลขที่ %s' %(searchcontractid)
+        elif 'search2' in request.POST and searchoname != '' and searchosur != '' and searchlicense != '':
+            cusbuycoumpulsory = Compulsory_Insurance.objects.filter(contract_id__in=cusbuycontractlistid, contract__status='Available', contract__car__license_on=searchlicense, contract__car__owner__fname=searchoname, contract__car__owner__lname=searchosur)
+            msg = 'ค้นหากรมธรรม์ทะเบียนรถ %s ผู้เอาประกัน %s %s' %(searchlicense, searchoname, searchosur)
 
     context = {
         'cusbuycoumpulsory': cusbuycoumpulsory,
-        'cusbuyinsurance': cusbuyinsurance
+        'msg': msg
     }
-    return render(request, 'contract.html', context=context)
-
+    return render(request, 'compulsory_insurance/search_compulsory.html', context=context)
 
 
 
@@ -213,3 +268,5 @@ def getcar(request):
     }
     
     return JsonResponse(response, status=200)
+
+# -----------------
