f740552740df827e17cde0444a79d9a54ae06618
ploidplanp <waoekpr@gmail.com>
2020-04-29 11:50:51 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/ploidplanp/carins""
    This reverts commit 2ef82cd6a4dc008da53efd7dad4cfb7b5b44825e.

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('company/', views.company_detail, name='company_detail'),
     path('car-use-type/', views.cartype_detail, name='cartype_detail'),
     path('premium-table/', views.premium_detail, name='premium_detail'),
-    path('mycustomer/', views.mycustomer, name='mycustomer'),
+    # path('mycustomer/', views.mycustomer, name='mycustomer'),
 ]
--- mysite/infocustomer/admin.py
+++ mysite/infocustomer/admin.py
@@ -1,14 +1,4 @@
 from django.contrib import admin
-from infocustomer.models import Seller
 from contract.models import Customer
 
 # Register your models here.
-
-
-admin.site.register(Seller)
-class PriceInline(admin.TabularInline):
-    model = Seller
-    extra = 0
-
-class FoodAdmin(admin.ModelAdmin):
-    inlines = [PriceInline]
